f49caa3ae6076b0d4975f1887cdb8c4b450b1820
open.grieves@gmail.com <sudo@redhat.com>
2024-04-24 22:16:47 +0200

    inventory fix

--- scripts/ansible_hosts.sh
+++ scripts/ansible_hosts.sh
@@ -5,7 +5,16 @@
 install_dir="/home/mglantz/advanced-networking-workshop"
 inventory_file="$install_dir/inventory"
 
-echo "[linux]" >$inventory_file
+cat << 'EOF' >$inventory_file
+[all:vars]
+# common variables
+ansible_user=admin
+ansible_ssh_private_key_file=~/.ssh/advanced-networking-workshop_id_rsa
+ansible_network_os=arista.eos.eos
+ansible_connection=ansible.netcommon.httpapi
+EOF
+
+echo "[linux]" >>$inventory_file
 sudo containerlab inspect -t $install_dir/containerlab/ceos-evpn-overlaid.clab.yml 2>/dev/null|cut -d'|' -f3,8|grep clab-EVPN | grep host|sed -e "s@/24@@g" -e "s/ //g" -e "s/|/ ansible_host=/g" >>$inventory_file
 
 echo "" >>$inventory_file
