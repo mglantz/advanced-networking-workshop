8f81e2c4a673b0bca60da0095e8c2ac5a0d996ef
open.grieves@gmail.com <sudo@redhat.com>
2024-04-24 20:59:07 +0200

    script to convert containerlab output to suitable inventory file

---
+++ scripts/ansible_hosts.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# Create Ansible inventory from the containerlab output
+# Magnus Glantz, sudo@redhat.com, 2024
+
+install_dir="/home/mglantz/advanced-networking-workshop"
+inventory_file="$install_dir/inventory"
+
+echo "[linux]" >$inventory_file
+sudo containerlab inspect -t $install_dir/containerlab/ceos-evpn-overlaid.clab.yml 2>/dev/null|cut -d'|' -f3,8|grep clab-EVPN | grep host|sed -e "s@/24@@g" -e "s/ //g" -e "s/|/ ansible_host=/g" >>$inventory_file
+
+echo "" >>$inventory_file
+echo "[leafs]" >>$inventory_file
+sudo containerlab inspect -t $install_dir/containerlab/ceos-evpn-overlaid.clab.yml 2>/dev/null|cut -d'|' -f3,8|grep clab-EVPN | sed -e "s@/24@@g" -e "s/ //g" -e "s/|/ ansible_host=/g"|grep leaf >>$inventory_file
+
+echo "" >>$inventory_file
+echo "[spines]" >>$inventory_file
+sudo containerlab inspect -t $install_dir/containerlab/ceos-evpn-overlaid.clab.yml 2>/dev/null|cut -d'|' -f3,8|grep clab-EVPN | sed -e "s@/24@@g" -e "s/ //g" -e "s/|/ ansible_host=/g"|grep spine >>$inventory_file
+
+echo "" >>$inventory_file
+echo "[switches:children]" >>$inventory_file
+echo "leafs" >>$inventory_file
+echo "spines" >>$inventory_file
