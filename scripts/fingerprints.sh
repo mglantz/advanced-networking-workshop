82b3d48fad4d0e8a7dd17b91183d40b12b78bfe1
open.grieves@gmail.com <sudo@redhat.com>
2024-05-10 16:36:13 +0200

    directory fix

--- scripts/fingerprints.sh
+++ scripts/fingerprints.sh
@@ -2,7 +2,7 @@
 # Accept fingerprints based on ansible inventory
 # Magnus Glantz, sudo@redhat.com, 2024
 
-install_dir="/home/mglantz/advanced-networking-workshop"
+install_dir="$HOME/advanced-networking-workshop"
 inventory_file="$install_dir/inventory"
 
 cat $inventory_file | grep host | cut -d '=' -f 2 | xargs ssh-keyscan -H >> ~/.ssh/known_hosts
